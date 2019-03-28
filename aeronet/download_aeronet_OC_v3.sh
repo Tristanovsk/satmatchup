75e04105eced3faf64b6e94edf55249a3c0d06f8
Tristan Harmel <tristan.harmel@get.omp.eu>
2019-03-28 14:02:37 +0100

    add aeonet data and downloader

--- aeronet/download_aeronet_OC_v3.sh
+++ aeronet/download_aeronet_OC_v3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+odir='../OCv3/'
 
 year=2015 #02
 
@@ -12,7 +12,7 @@ do
  for site in 'Abu_Al_Bukhoosh' 'ARIAKE_TOWER' 'Bari_Waterfront' 'Blyth_NOAH' 'COVE_SEAPRISM' 'Gageocho_Station' 'Galata_Platform' 'Gloria' 'GOT_Seaprism' 'Gustav_Dalen_Tower' 'Helsinki_Lighthouse' 'Ieodo_Station' 'Irbe_Lighthouse' 'KAUST_Campus' 'Lake_Erie' 'Lake_Okeechobee' 'LISCO' 'Lucinda' 'MVCO' 'Palgrunden' 'Socheongcho' 'South_Greenbay' 'Thornton_C-power' 'USC_SEAPRISM' 'USC_SEAPRISM_2' 'Venise' 'WaveCIS_Site_CSI_6' 'Zeebrugge-MOW1'
 do
 echo $site
-wget  -qO- --no-check-certificate "https://aeronet.gsfc.nasa.gov/cgi-bin/print_web_data_v3?site="$site"&year="$year"&month=1&day=1&year2="$year2"&month2="$month2"&day2="$day2"&LWN"$level"=1&AVG=10" | sed -e 's/<[^>]*>//g' > ../OCv3/$site"_OCv3".lev$level
+wget  -qO- --no-check-certificate "https://aeronet.gsfc.nasa.gov/cgi-bin/print_web_data_v3?site="$site"&year="$year"&month=1&day=1&year2="$year2"&month2="$month2"&day2="$day2"&LWN"$level"=1&AVG=10" | sed -e 's/<[^>]*>//g' > $odir$site"_OCv3".lev$level
 
 done
 done
